9e83a68a4d59e10d47352c448ea1035a80fcd093
Rafaelps8 <rafaelsampaio5@hotmail.com>
2021-06-25 14:17:11 -0300

    Meu quinto commit

--- main.js
+++ main.js
@@ -30,3 +30,31 @@ header.classList.add('scroll')
     header.classList.remove('scroll')
   }
 })
+
+/* testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container',{
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+
+});
+
+/* ScrollReveal: mostrar elementos quando der scroll na pagina */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true,
+})
+
+scrollReveal.reveal(`
+#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testiomonials .testimonials,
+#contact .text, #contact .links
+`, 
+{interval: 100})
